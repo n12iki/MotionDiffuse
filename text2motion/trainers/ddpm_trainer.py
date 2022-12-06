a47cb9ad865ab471447634dd156d2755ed7d41fb
n12iki <p12ain@gmail.com>
2022-12-06 21:57:57 +0100

    Update ddpm_trainer.py

--- text2motion/trainers/ddpm_trainer.py
+++ text2motion/trainers/ddpm_trainer.py
@@ -189,14 +189,24 @@ class DDPMTrainer(object):
 
         start_time = time.time()
 
-        train_loader = build_dataloader(
-            train_dataset,
-            samples_per_gpu=self.opt.batch_size,
-            drop_last=True,
-            workers_per_gpu=4,
-            shuffle=True,
-            dist=self.opt.distributed,
-            num_gpus=0)#1)#len(self.opt.gpu_id))
+        try:
+            train_loader = build_dataloader(
+                train_dataset,
+                samples_per_gpu=self.opt.batch_size,
+                drop_last=True,
+                workers_per_gpu=4,
+                shuffle=True,
+                dist=self.opt.distributed,
+                num_gpus=len(self.opt.gpu_id))
+        except:
+            train_loader = build_dataloader(
+                train_dataset,
+                samples_per_gpu=self.opt.batch_size,
+                drop_last=True,
+                workers_per_gpu=4,
+                shuffle=True,
+                dist=self.opt.distributed,
+                num_gpus=1)
 
         logs = OrderedDict()
         for epoch in range(cur_epoch, self.opt.num_epochs):
