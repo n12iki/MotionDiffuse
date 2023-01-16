b8f35c5988a435c64808457f41807e3c429471ee
n12iki <p12ain@gmail.com>
2023-01-16 22:09:27 +0100

    Update visualization.py

--- text2motion/visualization.py
+++ text2motion/visualization.py
@@ -101,5 +101,6 @@ if __name__ == '__main__':
                     testSet[i]["output"]=motion
                     #title = args.text + " #%d" % motion.shape[0]
                     #plot_t2m(motion, args.result_path, args.npy_path, title)
+                    break
                 with open("/content/drive/MyDrive/Output/Position_MSE_output.json") as output_file:
                     json.dump(testSet,output_file)
