9a737ac1268a782d98f1dd97d63c7caf13734198
n12iki <p12ain@gmail.com>
2023-01-17 16:07:41 +0100

    Update visualization.py

--- text2motion/visualization.py
+++ text2motion/visualization.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
                         with open("/content/drive/MyDrive/Output/"+str(args.result_path),"w") as output_file:
                             json.dump(testSet,output_file)
                         output_file.close()
-                     count=count+1
+                    count=count+1
                     #title = args.text + " #%d" % motion.shape[0]
                     #plot_t2m(motion, args.result_path, args.npy_path, title)s
                 with open("/content/drive/MyDrive/Output/"+str(args.result_path),"w") as output_file:
